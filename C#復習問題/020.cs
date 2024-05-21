f047b26791ca49980e89fd4400404f7f28c9943d
k10 <112364360+n-k10@users.noreply.github.com>
2024-05-21 09:47:20 +0900

    Create 020.cs

---
+++ C#復習問題/020.cs
@@ -0,0 +1,12 @@
+int[] scores = {80, 95, 100, 70, 85};
+foreach (int score in scores)
+{
+    if (score >= 90)
+    {
+        Console.WriteLine("High score!");
+    }
+    else
+    {
+        Console.WriteLine("Keep trying!");
+    }
+}
