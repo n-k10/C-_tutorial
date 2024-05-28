8afda0699fbb15179d743041daf61f3596352eb9
k10 <112364360+n-k10@users.noreply.github.com>
2024-05-28 09:30:20 +0900

    Update 016.cs

--- C#復習問題/016.cs
+++ C#復習問題/016.cs
@@ -1,5 +1,15 @@
 int[] numbers = {5, 10, 15, 20, 25};
 int sum = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    sum += numbers[i];
+}
+Console.WriteLine(sum);
+
+
+#別解
+int[] numbers = {5, 10, 15, 20, 25};
+int sum = 0;
 foreach (int number in numbers)
 {
     sum += number;
