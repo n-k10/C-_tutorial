528b857120c6a266206b7e1143fff5707dc99efc
k10 <112364360+n-k10@users.noreply.github.com>
2024-05-28 09:29:36 +0900

    Update 013.cs

--- C#復習問題/013.cs
+++ C#復習問題/013.cs
@@ -1,5 +1,17 @@
 void PrintNumbers(int[] numbers)
 {
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.WriteLine(numbers[i]);
+    }
+}
+
+PrintNumbers(new int[]{1, 2, 3, 4, 5});
+
+
+#別解
+void PrintNumbers(int[] numbers)
+{
     foreach (int number in numbers)
     {
         Console.WriteLine(number);
