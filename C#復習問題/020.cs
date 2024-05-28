c55626f3919c9c1a5677e921e860d461a80fc439
k10 <112364360+n-k10@users.noreply.github.com>
2024-05-28 10:35:19 +0900

    Update 020.cs

--- C#復習問題/020.cs
+++ C#復習問題/020.cs
@@ -1,4 +1,19 @@
 int[] scores = {80, 95, 100, 70, 85};
+for (int i = 0; i < scores.Length; i++)
+{
+    if (scores[i] >= 90)
+    {
+        Console.WriteLine("High score!");
+    }
+    else
+    {
+        Console.WriteLine("Keep trying!");
+    }
+}
+
+
+#別解
+int[] scores = {80, 95, 100, 70, 85};
 foreach (int score in scores)
 {
     if (score >= 90)
